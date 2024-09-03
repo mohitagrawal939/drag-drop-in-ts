11274ff2f0e706fe92836142df57d366054815dc
Mohit Agrawal <mohitagrawal939@gmail.com>
2024-09-03 17:27:56 +0530

    added clear input method

--- src/app.ts
+++ src/app.ts
@@ -49,12 +49,19 @@ class ProjectInput {
         }
     }
 
+    private clearInputs() {
+        this.titleInputElement.value = '';
+        this.descriptionInputElement.value = '';
+        this.peopleInputElement.value = '';
+    }
+
     @autobind
     private submitHandler(event: Event) {
         event.preventDefault();
         const userInput = this.gatherUserInput();
         if(Array.isArray(userInput)){
             const [title, desc, people] = userInput;
+            this.clearInputs();
         }
     }
 
