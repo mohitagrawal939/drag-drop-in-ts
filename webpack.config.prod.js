c3e6d0431257c5fefb4b7c9d0ec3a854576bac71
Mohit Agrawal <mohitagrawal939@gmail.com>
2024-09-04 20:20:57 +0530

    added production webpack config

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -2,21 +2,13 @@ const path = require('path');
 const CleanPlugin = require('clean-webpack-plugin');
 
 module.exports = {
-    mode: 'development',
+    mode: 'production',
     entry: './src/app.ts',
-    devServer: {
-        static: [
-            {
-                directory: path.join(__dirname),
-            }
-        ],
-    },
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
-        publicPath: '/dist/',
     },
-    devtool: 'inline-source-map',
+    devtool: 'hidden-source-map',
     module: {
         rules: [
             {
